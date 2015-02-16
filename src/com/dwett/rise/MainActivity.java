210b89ea9f19663b46a313c0d6199d4095150422
David Wetterau <david.wetterau@gmail.com>
2015-02-15 20:00:33 -0600

    initial camera previewing

--- src/com/dwett/rise/MainActivity.java
+++ src/com/dwett/rise/MainActivity.java
@@ -1,27 +1,16 @@
 package com.dwett.rise;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.Window;
+import android.widget.FrameLayout;
 
-import java.util.*;
+import com.dwett.rise.camera.Preview;
+
 
 public class MainActivity extends Activity {
 
-    WifiManager wifi_service;
-    WifiScanReceiver receiver;
-    HashMap<String, Integer> initialLocationMap;
-    HashMap<String, Integer> currentLocationMap;
+    private Preview cameraPreview;
 
     /**
      * Called when the activity is first created.
@@ -29,9 +18,12 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        wifi_service = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         setContentView(R.layout.main);
+
+        cameraPreview = new Preview(this);
+        ((FrameLayout) findViewById(R.id.cameraPreview)).addView(cameraPreview);
 
         initializeHandlers();
     }
@@ -39,177 +31,14 @@ public class MainActivity extends Activity {
     @Override
     public void onResume() {
         super.onResume();
-        startWifiMonitor();
     }
 
     @Override
     public void onPause() {
-        stopWifiMonitor();
         super.onPause();
     }
 
     private void initializeHandlers() {
-        Button setLocationButton = (Button) findViewById(R.id.set_location);
-        setLocationButton.setOnClickListener(setLocationListener);
-    }
-
-    // 0.5 sec
-    private final static int WIFI_POLLING_INTERVAL = 500;
-
-    void startWifiMonitor() {
-        receiver = new WifiScanReceiver();
-        registerReceiver(receiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-
-        initialLocationMap = null;
-        currentLocationMap = null;
-
-        wifiScannerTask.run();
-    }
-
-    void stopWifiMonitor() {
-        unregisterReceiver(receiver);
-        wifiMonitorHandler.removeCallbacks(wifiCheckerTask);
-    }
-
-    private final View.OnClickListener setLocationListener = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View v) {
-            initialLocationMap = null;
-            currentLocationMap = null;
-        }
-    };
-
-    private final Handler wifiMonitorHandler = new Handler();
-
-    private final Runnable wifiScannerTask = new Runnable() {
-        @Override
-        public void run() {
-            wifi_service.startScan();
-        }
-    };
-
-    private final Runnable wifiCheckerTask = new Runnable() {
-        @Override
-        public void run() {
-            WifiInfo wifiInfo = wifi_service.getConnectionInfo();
-            List<ScanResult> wifiScanResults = wifi_service.getScanResults();
-            String wifiString = getStatusString(wifiInfo, wifiScanResults);
-
-            TextView textView = (TextView) findViewById(R.id.wifi_status);
-            textView.setText(wifiString);
-
-            updateLocationMaps(wifiScanResults);
-
-            wifiMonitorHandler.postDelayed(wifiScannerTask, WIFI_POLLING_INTERVAL);
-        }
-
-        private String getStatusString(WifiInfo wifiInfo, List<ScanResult> scanResults) {
-            StringBuilder sb = new StringBuilder();
-
-            // Print the info for this wifi station
-            sb.append("Connected to: \n");
-            sb.append(wifiInfo.getSSID());
-            sb.append(": ");
-            sb.append(wifiInfo.getRssi());
-            sb.append('\n');
-
-            // Sort the results
-            TreeSet<ScanResult> sortedResults = new TreeSet<ScanResult>(new Comparator<ScanResult>() {
-                @Override
-                public int compare(ScanResult scanResult, ScanResult scanResult2) {
-                    return (Integer.valueOf(scanResult2.level)).compareTo(scanResult.level);
-                }
-            });
-            sortedResults.addAll(scanResults);
-
-            sb.append("Nearby: \n");
-            for (ScanResult result : sortedResults) {
-                // Print out this result
-                sb.append(result.SSID);
-                sb.append(": ");
-                sb.append(result.level);
-                sb.append('\n');
-            }
-
-            return sb.toString();
-        }
-
-        private void updateLocationMaps(List<ScanResult> wifiScanResults) {
-            if (initialLocationMap == null) {
-                initialLocationMap = new HashMap<String, Integer>();
-                currentLocationMap = new HashMap<String, Integer>();
-                fillMap(initialLocationMap, wifiScanResults);
-            } else {
-                fillMap(currentLocationMap, wifiScanResults);
-                wifiMonitorHandler.post(compareLocation);
-            }
-        }
-
-        private void fillMap(Map<String, Integer> locationMap, List<ScanResult> wifiScanResults) {
-            locationMap.clear();
-            for (ScanResult result : wifiScanResults) {
-                locationMap.put(result.SSID, result.level);
-            }
-        }
-    };
-
-    private final Runnable compareLocation = new Runnable() {
-
-        @Override
-        public void run() {
-            // Compare the initial location to the current location and print if it's different
-            boolean different = this.isDifferent(initialLocationMap, currentLocationMap);
-            String locationText = (different) ? "Different place" : "Same place";
-
-            TextView textView = (TextView) findViewById(R.id.location_status);
-            textView.setText(locationText);
-        }
-
-        private boolean isDifferent(
-                Map<String, Integer> locationMap1, Map<String, Integer> locationMap2) {
-            Set<String> allKeys = new TreeSet<String>();
-
-            for (String key : locationMap1.keySet()) {
-                allKeys.add(key);
-            }
-            for (String key : locationMap2.keySet()) {
-                allKeys.add(key);
-            }
-            double differenceWeighting = 1000.0;
-            double missingPointValue = 300;
-            double missingPointDenominator = 4.0;
-            double total = 0.0;
-            double threshold = 20.0;
-
-            for (String key : allKeys) {
-                double value1, value2;
-                if (locationMap1.containsKey(key)) {
-                    value1 = locationMap1.get(key);
-                    if (locationMap2.containsKey(key)) {
-                        value2 = locationMap2.get(key);
-                    } else {
-                        value1 = Math.abs(value1);
-                        value2 = (missingPointValue + value1) / missingPointDenominator;
-                    }
-                } else {
-                    value2 = Math.abs(locationMap2.get(key));
-                    value1 = (missingPointValue + value2) / missingPointDenominator;
-                }
-                total += differenceWeighting * Math.abs((value2 - value1) / (value1 * value1));
-            }
-            TextView debugTextView = (TextView) findViewById(R.id.location_debug);
-            debugTextView.setText(total + " / " + threshold);
-
-            return total > threshold;
-        }
-    };
-
-    class WifiScanReceiver extends BroadcastReceiver {
-
-        @Override
-        public void onReceive(Context c, Intent intent) {
-            wifiMonitorHandler.post(wifiCheckerTask);
-        }
+        //Button setLocationButton = (Button) findViewById(R.id.set_location);
     }
 }
---
+++ src/com/dwett/rise/camera/Preview.java
@@ -0,0 +1,91 @@
+package com.dwett.rise.camera;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/**
+ * @author david
+ */
+public class Preview extends SurfaceView implements SurfaceHolder.Callback {
+
+    private SurfaceHolder surfaceHolder;
+
+    private static Camera camera = null;
+
+    public Preview(Context context) {
+        super(context);
+
+        surfaceHolder = this.getHolder();
+        surfaceHolder.addCallback(this);
+    }
+
+    public void attachCamera() {
+        camera = Camera.open();
+        if (camera != null) {
+            try {
+                camera.setPreviewDisplay(this.surfaceHolder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stopPreviewAndFreeCamera() {
+        if (camera != null) {
+            camera.stopPreview();
+            camera.release();
+
+            // Clear out all camera values
+            camera = null;
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // Preview surface exists, open the camera
+        attachCamera();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        if (camera != null) {
+            camera.setDisplayOrientation(90);
+            Camera.Parameters parameters = camera.getParameters();
+            parameters.set("rotation", 90);
+            parameters.set("orientation", "portrait");
+
+            int w = 0, h = 0;
+            for (Camera.Size s : parameters.getSupportedPreviewSizes()) {
+                if (s.height > h && s.height <= height && s.width > w && s.width <= width) {
+                    h = s.height;
+                    w = s.width;
+                }
+            }
+            parameters.setPreviewSize(w, h);
+            camera.setParameters(parameters);
+            camera.startPreview();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (camera != null) {
+            stopPreviewAndFreeCamera();
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        Paint p = new Paint(Color.RED);
+        canvas.drawText("Preview", canvas.getWidth() / 2, canvas.getHeight() / 2, p);
+    }
+}
